434df9c8a17a2a6f8f1c93e513e2efac02ae2207
Svendsen02 <sebastianthyregod@gmail.com>
2026-02-27 09:47:06 +0100

    Add test for passive mul

--- tests/Mpc.Tests/Tests.fs
+++ tests/Mpc.Tests/Tests.fs
@@ -34,3 +34,31 @@ let ``Addition`` () =
     let playersList = SecretShare.shareVm playersList
     
     Assert.True(checkSums playersList 30)
+
+[<Fact>]
+let ``Passive Mul`` () = 
+    let playersInt = 4
+
+    // Make a list of players
+    let playersList = List.init playersInt (fun k -> (PlayerModule.makePlayer k []))
+
+    //Split the numbers into shares
+    let sList = KofKshare.KShare 10 playersInt
+    //sList |> List.iter (fun (x) -> printf " %d " x)
+
+    let tList = KofKshare.KShare 20 playersInt
+    //tList |> List.iter (fun (x) -> printf " %d " x)
+
+    let adversaryStructure= SecrecyStructure.singletonSecretStructure playersList
+
+    let distributionList = SecretShare.tSetShare playersList adversaryStructure 
+
+    let playersList = SecretShare.distributeShares distributionList sList tList playersList
+
+    let Us = SecretShare.makeU playersInt playersList
+
+    let playersList = PassiveMul.passiveMul playersList Us
+
+    let playersList = SecretShare.shareVm playersList
+
+    Assert.True(checkSums playersList 200)
