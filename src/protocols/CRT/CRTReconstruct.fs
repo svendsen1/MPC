54abdf7f58b810c2781254c28bf0f7a739feab0a
Svendsen02 <sebastianthyregod@gmail.com>
2026-03-18 10:14:30 +0100

    Add CRT protocols

--- src/protocols/CRT/CRTReconstruct.fs
+++ src/protocols/CRT/CRTReconstruct.fs
@@ -1,5 +1,21 @@
 namespace Protocols
 
-module CRTReconstruct= 
+module CRTReconstruct =
     open MPCcore
-   
+    open Protocols
+
+    // Standard CRT reconstruction
+    // Given shares s_i and moduli p_i, find unique X < P_all
+    // such that X ≡ s_i (mod p_i) for all i
+    let crtReconstruct (shares: bigint list) (moduli: bigint list) : bigint =
+        let pAll = ExtendMath.product moduli
+
+        let terms =
+            List.map2 (fun s p ->
+                let mi = pAll / p           // P_all / p_i
+                let yi = ExtendMath.modInverse mi p    // mi^-1 mod p_i
+                s * mi * yi                 // s_i * M_i * y_i
+            ) shares moduli
+
+        let sum = List.fold (+) 0I terms
+        ((sum % pAll) + pAll) % pAll        // reduce mod P_all, ensure positive
--- src/protocols/CRT/CRTShare.fs
+++ src/protocols/CRT/CRTShare.fs
@@ -3,4 +3,43 @@ namespace Protocols
 module CRTShare= 
     open MPCcore
 
-    
+    // Verify parameters are valid before using
+    let verifyParams (p: CrtShareParams) : bool =
+        let pAll = ExtendMath.product p.Moduli
+        let correctnessHolds = (p.L + 1I) * p.P0 < pAll
+        
+        // Check pairwise coprimality
+        let allCoprime =
+            let allMods = p.P0 :: p.Moduli
+            List.forall (fun (a, b) ->
+                let (g, _, _) = ExtendMath.extGcd a b
+                g = 1I
+            ) [ for i in 0..allMods.Length-1 do
+                    for j in i+1..allMods.Length-1 do
+                        yield (allMods.[i], allMods.[j]) ]
+        
+        correctnessHolds && allCoprime
+
+    // Share a secret x in Z_p0
+    // Returns list of n shares, one per party
+    let share (x: bigint) (p: CrtShareParams) : bigint list =
+        if x < 0I || x >= p.P0 then
+            failwith "Secret out of range"
+        
+        // Pick random u in {0, ..., L-1}
+        let u = ExtendMath.randomBigint p.L
+        
+        // Lift x to integer X = x + u * p0
+        let X = x + u * p.P0
+        
+        // Each party i gets X mod p_i
+        List.map (fun pi -> X % pi) p.Moduli
+
+    // Reconstruct secret from shares
+    // Returns x in Z_p0
+    let reconstruct (shares: bigint list) (p: CrtShareParams) : bigint =
+        // Recover X via CRT
+        let X = CRTReconstruct.crtReconstruct shares p.Moduli
+        
+        // Reduce mod p0 to get secret back
+        X % p.P0
