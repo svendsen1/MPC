d92f0ee5b5a9a078ffd751ed3ff7b68aefb72f01
laurits105 <laurits.r.madsen@gmail.com>
2026-03-27 11:32:31 +0100

    implement reformat

--- src/protocols/CRT/CRTOnline.fs
+++ src/protocols/CRT/CRTOnline.fs
@@ -12,6 +12,21 @@ module CRTOnline =
         [1I..n-1I] |> List.map factorial 
                    |> List.fold (*) 1I
 
-
+    // Using CRT-reconstruct to find an x that holds for both:
+    // xBar = x (mod p0)
+    // xBar = 0 (mod d)
+    // CRT-Reconstruct returns an x for which it holds that all remainders 
+    // are the same. 
+    // Also this holds 
+    // 0 <= xBar <= p0 * d
+    let reFormat (x: bigint) (p0: bigint) (d: bigint): bigint =
+        let l1 = [x;0I] 
+        let l2 = [p0; d]
+        let xBar = CRTReconstruct.crtReconstruct l1 l2 
+        if 0I <= xBar && xBar <= (p0*d) then
+            xBar
+        else
+            failwith "Unable to reFormat"
+    
 
 
